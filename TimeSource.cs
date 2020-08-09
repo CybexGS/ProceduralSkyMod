0676e5db1fe902db3f568ee7efadbcbdb04233c1
CybexGS <cybexgamestudio@gmail.com>
2020-08-10 00:57:47 +0200

    Crude day length calculation

--- TimeSource.cs
+++ TimeSource.cs
@@ -5,14 +5,13 @@ namespace ProceduralSkyMod
 {
 	public static class TimeSource
 	{
-		private static float dayLengthInSeconds = Main.settings.dayLengthSeconds;
 		private static float dayProgress = 0f;
 
 		public static float DayProgressDelta { get { return CalculateDayProgress(); } }
 
 		public static float CalculateDayProgress ()
 		{
-			float newProgress = (Time.time % dayLengthInSeconds) / dayLengthInSeconds;
+			float newProgress = (Time.time % Main.settings.dayLengthSeconds) / Main.settings.dayLengthSeconds;
 			float delta = (newProgress < dayProgress) ? newProgress + 1 - dayProgress : newProgress - dayProgress;
 			dayProgress = newProgress;
 			return delta;
